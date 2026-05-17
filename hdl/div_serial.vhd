fa072cc370155f24d2f77520360a89c3690f54a7
Robin Lux <studium@robinlux.de>
2026-05-17 09:59:15 +0200

    added synthesisable mul/div implementation

---
+++ hdl/div_serial.vhd
@@ -0,0 +1,104 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+
+entity div_serial is
+    generic(
+        WIDTHS          :     positive := 32
+    ); 
+    port (
+        clk, res_n    : in    std_logic;
+
+        calc          : in    boolean;
+
+        a             : in    std_logic_vector(WIDTHS - 1 downto 0);
+        b             : in    std_logic_vector(WIDTHS - 1 downto 0);
+
+        quot          : out   std_logic_vector(WIDTHS - 1 downto 0);
+        remain        : out   std_logic_vector(WIDTHS - 1 downto 0);
+
+        busy          : out   boolean
+    );
+end entity div_serial;
+
+architecture behav of div_serial is
+    signal start_mul: boolean;
+    signal m_done : boolean;
+    signal m_start : boolean;
+    signal m_abort: boolean;
+
+    signal m_r, next_m_r: std_logic_vector(31 downto 0);
+    signal m_r_shifted: std_logic_vector(32 downto 0);
+    signal m_q, next_m_q: std_logic_vector(b'range);
+    signal m_cycle, next_m_cycle: natural range 0 to 33;
+
+    signal q, next_q: std_logic;
+    signal alur: std_logic_vector(32 downto 0);
+    signal invcarry: std_logic;
+begin
+    m_start <= calc;
+
+    mul_state_p: process(clk, res_n) is
+    begin
+        if res_n /= '1' then
+            m_q <= (others => '0');
+            m_r <= (others => '0');
+            m_cycle <= 33;
+            q      <= '0';
+        else
+            if (clk'event and clk = '1') then  
+                m_q         <= next_m_q;
+                m_r         <= next_m_r;
+                m_cycle     <= next_m_cycle;
+                q           <= next_q;
+            end if;
+        end if;
+    end process mul_state_p;
+
+
+    alur   <= std_logic_vector(unsigned('0' & m_r)   + unsigned(b)) when q = '0' and m_cycle = 32 else
+              std_logic_vector(unsigned(m_r_shifted) + unsigned(b)) when q = '0' else
+              std_logic_vector(unsigned(m_r_shifted) - unsigned(b));
+
+    invcarry <= not alur(32);
+
+    m_r_shifted <= m_r & m_q(31);
+
+
+    m_done <= m_cycle = 0;
+    m_abort <= false;
+    busy   <= (m_cycle /= 0 and m_cycle /= 33) or (m_cycle = 0 and m_start);
+    mul_calc_p: process(all) is
+    begin
+        next_m_cycle    <= m_cycle;
+        next_m_r        <= (others => 'X');
+        next_m_q        <= (others => 'X');
+        next_q          <= invcarry;
+
+        if m_cycle = 33 and not m_start then
+            next_m_cycle <= 0;
+        end if;
+
+        if m_start and m_cycle = 0 then
+            next_m_cycle        <= 1;
+            next_m_r            <= (others => '0');
+            next_m_q            <= a(30 downto 0) & '0';
+            next_q              <= '1';
+        else
+            if m_cycle /= 0 and m_cycle /= 32 and m_cycle /= 33 then
+                next_m_q        <= m_q(30 downto 0) & invcarry;
+                next_m_r        <= alur(31 downto 0);
+                next_m_cycle    <= m_cycle + 1;
+            elsif m_cycle = 32 then
+                next_m_r        <= alur(31 downto 0);
+                next_m_q        <= m_q;
+                next_m_cycle    <= m_cycle + 1;
+            end if; 
+        end if;
+    end process mul_calc_p;
+
+    quot <= m_q;
+    remain <= m_r;
+end architecture behav;
+
---
+++ hdl/mult_serial.vhd
@@ -0,0 +1,94 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+
+entity mult_serial is
+    generic(
+        WIDTHS          :     positive := 32
+    ); 
+    port (
+        clk, res_n    : in    std_logic;
+
+        calc          : in    boolean;
+
+        a             : in    std_logic_vector(WIDTHS - 1 downto 0);
+        b             : in    std_logic_vector(WIDTHS - 1 downto 0);
+
+        result        : out   std_logic_vector(2*WIDTHS - 1 downto 0);
+
+        busy          : out   boolean
+    );
+end entity mult_serial;
+
+architecture behav of mult_serial is
+    signal start_mul: boolean;
+    signal next_m_accu, m_accu: std_logic_vector(result'range);
+    signal m_done : boolean;
+    signal m_start : boolean;
+    signal m_abort: boolean;
+
+    signal m_a, next_m_a: std_logic_vector(result'range);
+    signal m_b, next_m_b: std_logic_vector(b'range);
+    signal m_cycle, next_m_cycle: natural range 0 to 33;
+begin
+    m_start <= calc;
+
+
+    mul_state_p: process(clk, res_n) is
+    begin
+        if res_n /= '1' then
+            m_a <= (others => '0');
+            m_b <= (others => '0');
+            m_accu <= (others => '0');
+            m_cycle <= 33;
+        else
+            if (clk'event and clk = '1') then  
+                m_a         <= next_m_a;
+                m_b         <= next_m_b;
+                m_accu      <= next_m_accu;
+                m_cycle     <= next_m_cycle;
+            end if;
+        end if;
+    end process mul_state_p;
+
+
+    m_done <= m_cycle = 0;
+    m_abort <= m_b = x"00000000";
+    busy   <= (m_cycle /= 0 and m_cycle /= 33) or (m_cycle = 0 and m_start);
+    mul_calc_p: process(all) is
+    begin
+        next_m_cycle    <= m_cycle;
+        next_m_accu     <= m_accu;
+        next_m_a        <= (others => 'X');
+        next_m_b        <= (others => 'X');
+
+        if m_cycle = 33 and not m_start then
+            next_m_cycle <= 0;
+        end if;
+
+        if m_start and m_cycle = 0 then
+            next_m_cycle        <= 1;
+            next_m_a            <= (others => '0');
+            next_m_b            <= b;
+            next_m_a(a'range)   <= a;
+            next_m_accu         <= (others => '0');
+        else
+            if m_cycle /= 0 and m_cycle /= 33 then
+                next_m_a        <= m_a(m_a'left - 1 downto 0) & '0';
+                next_m_accu     <= std_logic_vector(unsigned(m_accu) + unsigned(m_a)) when m_b(0) = '1' else
+                                   next_m_accu;
+                next_m_b        <= '0' & m_b(m_b'left downto 1);
+
+                if m_cycle = 32 or m_abort then
+                    next_m_cycle <= 33;
+                else
+                    next_m_cycle    <= m_cycle + 1;
+                end if;
+            end if; 
+        end if;
+    end process mul_calc_p;
+
+    result <= m_accu;
+end architecture behav;
+
